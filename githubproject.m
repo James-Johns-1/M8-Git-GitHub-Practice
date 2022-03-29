fec958f37d304b5457dd995fce8c0c7f6fec7a4d
maxwelljb123 <102571018+maxwelljb123@users.noreply.github.com>
2022-03-29 12:00:51 -0400

    Add files via upload

---
+++ githubproject.m
@@ -0,0 +1,7 @@
+%this part of code defines angles for 3 dimensions
+sidea=((4^2)+(3^2))^(1/2);
+angley=tan(3/4);
+anglex=tan(sidea/2);
+%these are the vector at the xy and xz vectors
+xz=120*(anglex)
+yz=120*(angley)
